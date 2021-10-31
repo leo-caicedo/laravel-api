a4f00fc644a384152dddd77e8c516852429e9ce1
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-10-30 20:32:36 -0500

    Introducción a la API privada

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
 
-// V1
-Route::apiResource('v1/posts', PostV1::class);
-// V2
-Route::apiResource('v2/posts', PostV2::class);
+ //V1
+//Route::apiResource('v1/posts', PostV1::class)
+    //->middleware('auth:sanctum');
+ //V2
+//Route::apiResource('v2/posts', PostV2::class)
+    //->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    // V1
+    Route::apiResource('v1/posts', PostV1::class);
+    // V2
+    Route::apiResource('v2/posts', PostV2::class);
+});
