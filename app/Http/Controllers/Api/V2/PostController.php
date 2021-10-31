d969b2b79ee2e2c0c1c003667f521fbf227189b9
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-10-30 20:19:15 -0500

    Versión 2: colección

--- app/Http/Controllers/Api/V2/PostController.php
+++ app/Http/Controllers/Api/V2/PostController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers\Api\V2;
 
 use App\Http\Controllers\Controller;
-use App\Models\Post;
 use Illuminate\Http\Request;
+// models
+use App\Models\Post;
 // resource
 use App\Http\Resources\V2\PostResource;
 use App\Http\Resources\V2\PostCollection;
@@ -18,7 +19,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return new PostCollection(Post::latest()->paginate());
     }
 
     /**
--- app/Http/Resources/V2/PostCollection.php
+++ app/Http/Resources/V2/PostCollection.php
@@ -14,6 +14,13 @@ class PostCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'organization' => 'YoMiMeConMigoCORP',
+                'author' => 'Said Leonardo'
+            ],
+            'type' => 'articles'
+        ];
     }
 }
