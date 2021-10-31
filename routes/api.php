57af6c8711bce6e704689c56470f7bede18b378c
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-10-30 19:38:12 -0500

    Versión 2: planificación y configuración inicial

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class);
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
+
+// V1
+Route::apiResource('v1/posts', PostV1::class);
+// V2
+Route::apiResource('v2/posts', PostV2::class);
